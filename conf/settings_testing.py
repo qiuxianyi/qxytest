73b15ce8a0f4498e51e03b8dffe443571878c998
EatingWu <10ytwu@gmail.com>
2018-09-01 00:16:36 +0800

    'update'

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+        'NAME': 'mydb_qxy',                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'root',                        # 你的数据库user
+        'PASSWORD': 'OIraQ3SJ_x',                        # 你的数据库password
+        'HOST': '172.50.22.22',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
